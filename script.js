0b92cfff29b48b9fe392785aa2432bc142f5df15
Ayal Gelles <ayalgelles@ayals-air.mynet>
2016-06-04 20:28:54 +0300

    njs

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import React from 'react'
 import { render } from 'react-dom'
 var _ = require('lodash');
 
+import 'whatwg-fetch';
+
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
 
 
